fcd50091f621cfc739da855de777a8a6cff4801c
Shreya Shankar <shreya1@stanford.edu>
2016-05-24 20:15:08 -0700

    with lab7

--- lab7.c
+++ lab7.c
@@ -6,6 +6,11 @@
 #include "console.h"
 #include "interrupts.h"
 
+volatile static unsigned counter20 = 0;
+volatile static unsigned counter21 = 0;
+volatile static cir_t *buffer20;
+volatile static cir_t *buffer21;
+
 void int_handler(unsigned);
 
 // should not get called.
@@ -19,25 +24,59 @@ void interrupt_vector(unsigned pc) {
 }
 
 void int_handler(unsigned pc) {
-  gpio_check_and_clear_event(GPIO_PIN21);
+  if (gpio_check_and_clear_event(GPIO_PIN21)) {
+    counter21++;
+    cir_enqueue(buffer21, counter21);
+  }
+  if (gpio_check_and_clear_event(GPIO_PIN20)) {
+    counter20++;
+    cir_enqueue(buffer20, counter20);
+  }
+  //cir_enqueue(buffer, counter);
+
+  
+  //printf("%d ", counter);
+ 
 }
 
 void button_test() {
+  gpio_set_function(GPIO_PIN21, GPIO_FUNC_INPUT);
+  gpio_set_pullup(GPIO_PIN21);
+  while (gpio_read(GPIO_PIN21) == 1);
+  printf("+");
+  while (gpio_read(GPIO_PIN21) == 0);
 
-	gpio_
 }
 
-/*
+
 static void setup_interrupts() {
+    gpio_set_function(GPIO_PIN21, GPIO_FUNC_INPUT);
+    gpio_set_pullup(GPIO_PIN21);
     gpio_detect_falling_edge(GPIO_PIN21);
+    gpio_detect_rising_edge(GPIO_PIN21);
+    gpio_set_function(GPIO_PIN20, GPIO_FUNC_INPUT);
+    gpio_set_pullup(GPIO_PIN20);
+    gpio_detect_falling_edge(GPIO_PIN20);
+    gpio_detect_rising_edge(GPIO_PIN20);
     interrupts_enable(INTERRUPTS_GPIO3);
     system_enable_interrupts();
 }
-*/ 
+
 
 void main(void) {
+  buffer20 = cir_new();
+  buffer21 = cir_new();
   printf_init();
+  setup_interrupts();
   while (1) {
-    // Do nothing, for now.
+    if (!cir_empty(buffer20)) {
+      printf("20: %d\n", cir_dequeue(buffer20));
+    }
+    if (!cir_empty(buffer21)) {
+      printf("21: %d\n", cir_dequeue(buffer21));
+    }
+    
+    //printf("%d\n", counter);
+    //button_test()
   }
 }
