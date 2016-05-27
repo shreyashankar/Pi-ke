3a1c8158a2fbbbcb9652891fec021abe9c9bfc6f
Shreya Shankar <shreya1@stanford.edu>
2016-05-27 10:59:20 -0700

    working blinking lights

--- main.c
+++ main.c
@@ -14,11 +14,12 @@
 #include "LSM6DS33.h"
 
 #define MIN_INTERVAL 300000
-#define TIMER_INTERVAL 1000000
+#define TIMER_INTERVAL 500000
 
 static int left_on;
 static int right_on;
 static int current_time;
+static int interval_time;
 
 void indicator_handler(unsigned);
 void blink_handler(unsigned);
@@ -30,26 +31,28 @@ void impossible_vector(unsigned pc) {
 
 /* we only enable GPIO interrupts, so just forward it blindly  */
 void interrupt_vector(unsigned pc) {
-  // if (gpio_read(LEFT_INDICATOR_BUTTON_PIN) == 0 || gpio_read(RIGHT_INDICATOR_BUTTON_PIN) == 0) {
-  //   indicator_handler(pc);
-  // }
-  //else {
+  if (gpio_read(LEFT_INDICATOR_BUTTON_PIN) == 0 || gpio_read(RIGHT_INDICATOR_BUTTON_PIN) == 0) {
+    indicator_handler(pc);
+  }
+  else {
     blink_handler(pc);
-  //}
+  }
 }
 
 void blink_handler(unsigned pc) {
-  if (!left_on) {
+  if (timer_get_time() - interval_time < TIMER_INTERVAL) {
+    return;
+  }
+  interval_time = timer_get_time();
+  if (left_on) {
     if (gpio_read(LEFT_INDICATOR_LIGHT_PIN) == 0) {
-      printf("1 hi\n");
       gpio_write(LEFT_INDICATOR_LIGHT_PIN, 1);
     }
     else {
-      printf("2 hi\n");
       gpio_write(LEFT_INDICATOR_LIGHT_PIN, 0);
     }
   }
-  else if (!right_on) {
+  else if (right_on) {
     if (gpio_read(RIGHT_INDICATOR_LIGHT_PIN) == 0) {
       gpio_write(RIGHT_INDICATOR_LIGHT_PIN, 1);
     }
@@ -70,40 +73,28 @@ void indicator_handler(unsigned pc) {
   if (gpio_check_and_clear_event(LEFT_INDICATOR_BUTTON_PIN)) {
     if (right_on) {
       right_on = 0;
-      //armtimer_stop();
       gpio_write(RIGHT_INDICATOR_LIGHT_PIN, 0);
     }
     if (left_on) {
-      //turn off left armtimer
       left_on = 0;
-      //armtimer_stop();
       gpio_write(LEFT_INDICATOR_LIGHT_PIN, 0);
     }
     else {
-      //init left armtimer
       left_on = 1;
-      //armtimer_init();
-      //armtimer_start(TIMER_INTERVAL);
-      //gpio_write(LEFT_INDICATOR_LIGHT_PIN, 1);
     }
   }
 
-  if (gpio_check_and_clear_event(RIGHT_INDICATOR_BUTTON_PIN)) {
+  else if (gpio_check_and_clear_event(RIGHT_INDICATOR_BUTTON_PIN)) {
     if (left_on) {
       left_on = 0;
-      //armtimer_stop();
       gpio_write(LEFT_INDICATOR_LIGHT_PIN, 0);
     }
     if (right_on) {
       right_on = 0;
-      //armtimer_stop();
       gpio_write(RIGHT_INDICATOR_LIGHT_PIN, 0);
     }
     else {
       right_on = 1;
-      //armtimer_init();
-      //armtimer_start(TIMER_INTERVAL);
-      //gpio_write(RIGHT_INDICATOR_LIGHT_PIN, 1);
     }
   }
   current_time = timer_get_time();
@@ -137,13 +128,14 @@ void main(void) {
   //TESTING ARMTIMER
   gpio_set_function(LEFT_INDICATOR_LIGHT_PIN, GPIO_FUNC_OUTPUT);
   gpio_set_function(RIGHT_INDICATOR_LIGHT_PIN, GPIO_FUNC_OUTPUT);
+  setup_interrupts();
 
   printf_init();
-  armtimer_init(2000000); // 1s 
+  armtimer_init(1000000); // 1s 
   armtimer_enable(); 
 
   armtimer_enable_interrupt(); 
-  //interrupts_enable_basic(INTERRUPTS_BASIC_ARM_TIMER_IRQ);
+  interrupts_enable_basic(INTERRUPTS_BASIC_ARM_TIMER_IRQ);
   system_enable_interrupts();
  
   current_time = timer_get_time();
@@ -153,17 +145,17 @@ void main(void) {
 
   /***************************** MAY 25 WEDNESDAY LAB *****************************/
 
-  //MAGNET STUFF: see halleffect.c for doc. halleffect.c is never run
+  // //MAGNET STUFF: see halleffect.c for doc. halleffect.c is never run
 
-  /*gpio_set_function(HALL_PIN, GPIO_FUNC_INPUT);
-  gpio_set_pullup(HALL_PIN);
+  // gpio_set_function(HALL_PIN, GPIO_FUNC_INPUT);
+  // gpio_set_pullup(HALL_PIN);
 
-  for(int i = 0; i < 10; i++) {
-      while(gpio_read(HALL_PIN) == 1) {}
-    printf("magnet close!\n");
-      while(gpio_read(HALL_PIN) == 0) {}
-    printf("magnet out of range!\n");
-  }*/ 
+  // for(int i = 0; i < 10; i++) {
+  //     while(gpio_read(HALL_PIN) == 1) {}
+  //   printf("magnet close!\n");
+  //     while(gpio_read(HALL_PIN) == 0) {}
+  //   printf("magnet out of range!\n");
+  // } 
 
   //ACCELEROMETER STUFF
 
